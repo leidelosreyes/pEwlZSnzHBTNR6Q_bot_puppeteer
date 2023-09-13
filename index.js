e832dbb7dd2d2f14484cbfa98d8efcf4511814f0
leo <leidelosreyes060296@gmail.com>
2023-09-13 14:10:07 +0800

    initial commit

--- index.js
+++ index.js
@@ -3,103 +3,8 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 
 puppeteer.use(StealthPlugin());
 
-const ids = [
-  '6319867',
-  '6319825',
-  '6319824',
-  '6319823',
-  '6319821',
-  '6319820',
-  '6319819',
-  '6319818',
-  '6319817',
-  '6319816',
-  '6319811',
-  '6319810',
-  '6319810',
-  '6319808',
-  '6319807',
-  '6319806',
-  '6319803',
-  '6319802',
-  '6319798',
-  '6318197',
-  '6320400',
-  '6320399',
-  '6320398',
-  '6320397',
-  '6320396',
-  '6320392',
-  '6320391',
-  '6320390',
-  '6320388',
-  '6320387',
-  '6320386',
-  '6320385',
-  '6320384',
-  '6320383',
-  '6320382',
-  '6320382',
-  '6320382',
-  '6320378',
-  '6320377',
-  '6320375'
-];
-
-
-
-const targetLinks = [
-  'https://gloorsie.com/4/',
-  'https://almstda.tv/4/',
-  'https://thaudray.com/4/',
-  'https://grunoaph.net/4/',
-  'https://afodreet.net/4/',
-  'https://psuftoum.com/4/',
-  'https://whairtoa.com/4/',
-  'https://augailou.com/4/',
-  'https://ookroush.com/4/',
-  'https://nabauxou.net/4/',
-  'https://ptugnins.net/4/',
-  'https://eptougry.net/4/',
-  'https://itespurrom.com/4/',
-  'https://intorterraon.com/4/',
-  'https://shulugoo.net/4/',
-  'https://hoglinsu.com/4/',
-  'https://vasteeds.net/4/',
-  'https://potskolu.net/4/',
-  'https://vaikijie.net/4/',
-  'https://chalaips.com/4/',
-  'https://waufooke.com/4/',
-  'https://whoursie.com/4/',
-  'https://phomoach.net/4/',
-  'https://sotchoum.com/4/',
-  'https://afodreet.net/4/',
-  'https://lidsaich.net/4/',
-  'https://greewepi.net/4/',
-  'https://soocaips.com/4/',
-  'https://zirdough.net/4/',
-  'https://whulsaux.com/4/',
-  'https://dubzenom.com/4/',
-  'https://woafoame.net/4/',
-  'https://vaitotoo.net/4/',
-  'https://zeekaihu.net/4/',
-  'https://nabauxou.net/4/',
-  'https://zaltaumi.net/4/',
-  'https://keewoach.net/4/',
-  'https://thefacux.com/4/',
-  'https://atservineor.com/4/',
-  'https://ptaupsom.com/4/',
-  'https://atservineor.com/4/',
-  'https://psuftoum.com/4/',
-  'https://vaitotoo.net/4/',
-  'https://vasteeds.net/4/',
-  'https://dubzenom.com/4/',
-  'https://soocaips.com/4/',
-  'https://nabauxou.net/4/',
-  'https://lidsaich.net/4/',
-  'https://intorterraon.com/4/',
-  'https://meenetiy.com/4/'
-];
+const ids = require('./data/id.js');
+const targetLinks = require('./data/url.js');
 
 
 async function processLink(link) {
@@ -125,36 +30,46 @@ async function processLink(link) {
     while (true) {
       await page.goto(link, { waitUntil: 'domcontentloaded' });
       console.log(`Initial page loaded: ${link}`);
-      await page.waitForTimeout(3000);
+      await page.waitForTimeout(5000);
 
       const currentUrl = page.url();
-
+      page.waitForTimeout(3000);
       while (true) {
         const redirectedUrl = page.url();
-
         if (redirectedUrl === currentUrl) {
           console.log(`Reached final destination: ${redirectedUrl}`);
+
           const bodyHandle = await page.$('body');
-          const { x, y } = await bodyHandle.boundingBox();
-
-          // Calculate a point within the body and click it
-          const clickX = x + Math.random() * (x + 1);
-          const clickY = y + Math.random() * (y + 1);
-
-          await page.mouse.click(clickX, clickY);
-          console.log("page.mouse.click("+clickX+", "+clickY+")");
           await page.waitForTimeout(3000);
-          console.log('done: ' + redirectedUrl);
-
-          if (lastUrl === redirectedUrl) {
-            console.log(`Redirect loop detected. Breaking loop for link: ${link}`);
-            break;
-          } else {
-            lastUrl = redirectedUrl;
+          if (bodyHandle) {
+            const { x, y } = await bodyHandle.boundingBox();
+            // Calculate a point within the body and click it
+            const clickX = x + Math.random() * (x + 1);
+            const clickY = y + Math.random() * (y + 1);
+            const clickX1 = x + Math.random() * (x + 1);
+            const clickY1 = y + Math.random() * (y + 1);
+            await page.mouse.click(clickX, clickY);
+            await new Promise(resolve => setTimeout(resolve, 4000));
+            console.log("page.mouse.click(" + clickX + ", " + clickY + ")");
+            await new Promise(resolve => setTimeout(resolve, 4000));
+            await page.mouse.click(clickX1, clickY1);
+            console.log("page.mouse.click(" + clickX1 + ", " + clickY1 + ")");
+            await new Promise(resolve => setTimeout(resolve, 4000));
+            console.log('done: ' + redirectedUrl);
+            //await new Promise(resolve => setTimeout(resolve, 4000));
+            if (lastUrl === redirectedUrl) {
+              console.log(`Redirect loop detected. Breaking loop for link: ${link}`);
+              break;
+            } else {
+              lastUrl = redirectedUrl;
+            }
+          }
+          else {
+            console.error("bodyHandle is null");
           }
         }
-
         // Wait for 1 second before checking the URL again
+        console.log("cheking.........")
         await page.waitForTimeout(1000); // 1000 milliseconds = 1 second
       }
 
@@ -169,7 +84,7 @@ async function processLink(link) {
   }
 }
 
- // Random ids on array
+// Random ids on array
 function getRandomId() {
   const randomIndex = Math.floor(Math.random() * ids.length);
   return ids[randomIndex];
@@ -192,8 +107,9 @@ async function processAllLinks(links) {
   while (true) {
     for (const link of links) {
       const randomTargetLink = link + getRandomId();
-      console.log("Random Links : "+" "+randomTargetLink);
+      console.log("Random Links : " + " " + randomTargetLink);
       await processLink(randomTargetLink);
+      await new Promise(resolve => setTimeout(resolve, 4000)); // Wait for 1 minute
     }
   }
 }
